993d79e8a68745d4b59cc27076e5fae31031a187
tokuhirom <tokuhirom@gmail.com>
2011-01-24 11:43:27 +0900

    specify the configure options for vim. it fixes multibyte related issue.

--- spec/vim.pl
+++ spec/vim.pl
@@ -1,1 +1,8 @@
 url 'ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2';
+
+sub install {
+    configure(qw/--disable-gui --without-x --disable-xim --enable-multibyte/);
+    make();
+    make('install');
+}
+
